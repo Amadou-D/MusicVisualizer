8abe914287af2c7e369a1f50337899a524342eaf
Amadou <116417004+Amadou-D@users.noreply.github.com>
2025-02-13 00:17:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://music-visualizeer.vercel.app', // Replace with your Vercel app URL
+  methods: ['GET', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
 
 app.get('/audio', async (req, res) => {
   const videoUrl = req.query.url;
